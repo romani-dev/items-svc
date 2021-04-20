c28cef2724cd932e23fd916f2c361d29fa4cde1c
Felipe Romani <felipe.romani@wspot.com.br>
2021-04-20 17:09:54 -0300

    added images

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 type Item struct {
 	ID     int
 	Name   string
+	Image  string
 	UserID int
 }
 
@@ -20,9 +21,9 @@ func main() {
 		log.Printf("x-request-id: %v", requestID)
 		c.Header("x-request-id", requestID)
 		c.JSON(200, []Item{
-			{ID: 12, Name: "IPhone", UserID: 1},
-			{ID: 13, Name: "Dell XPS", UserID: 1},
-			{ID: 14, Name: "Monitor Samsung QLED", UserID: 1},
+			{ID: 12, Name: "IPhone", UserID: 1, Image: "https://picsum.photos/300/300"},
+			{ID: 13, Name: "Dell XPS", UserID: 1, Image: "https://picsum.photos/300/300"},
+			{ID: 14, Name: "Monitor Samsung QLED", UserID: 1, Image: "https://picsum.photos/300/300"},
 		})
 	})
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
