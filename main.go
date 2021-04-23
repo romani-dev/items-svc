82b125bdc43b28b647366ea4b50083fdfb2e2736
Felipe Romani <felipe.romani@wspot.com.br>
2021-04-23 17:55:37 -0300

    Error test

--- main.go
+++ main.go
@@ -20,10 +20,13 @@ func main() {
 		requestID := c.Request.Header.Get("x-request-id")
 		log.Printf("x-request-id: %v", requestID)
 		c.Header("x-request-id", requestID)
-		c.JSON(200, []Item{
-			{ID: 12, Name: "IPhone", UserID: 1, Image: "https://picsum.photos/300/300"},
-			{ID: 13, Name: "Dell XPS", UserID: 1, Image: "https://picsum.photos/300/300"},
-			{ID: 14, Name: "Monitor Samsung QLED", UserID: 1, Image: "https://picsum.photos/300/300"},
+		// c.JSON(200, []Item{
+		// 	{ID: 12, Name: "IPhone", UserID: 1, Image: "https://picsum.photos/300/300"},
+		// 	{ID: 13, Name: "Dell XPS", UserID: 1, Image: "https://picsum.photos/300/300"},
+		// 	{ID: 14, Name: "Monitor Samsung QLED", UserID: 1, Image: "https://picsum.photos/300/300"},
+		// })
+		c.JSON(500, gin.H{
+			"error": "error",
 		})
 	})
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
